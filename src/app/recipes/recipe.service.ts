be5967d8ec2bdc9eb677511025c9df415bb35882
kjdeepak <kjdeepak4work@gmail.com>
2019-12-21 00:01:32 +0530

    course project observables final

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,11 +1,10 @@
 import { Recipe } from './recipe.model';
-import { Injectable, EventEmitter } from '@angular/core';
+import { Injectable } from '@angular/core';
 import { Ingredient } from '../shared/ingredient.model';
 import { ShoppingListService } from '../shopping-list/shopping-list.service';
 
 @Injectable({providedIn: 'root'})
 export class RecipeService {
-    recipeSelected = new EventEmitter();
     
     recipes: Recipe[] = [
         new Recipe('A Test Recipe', 'This is simple a test', 'assets/recipe_logo.jpeg',[new Ingredient('frenchfries', 3), new Ingredient('buns',2)]),
